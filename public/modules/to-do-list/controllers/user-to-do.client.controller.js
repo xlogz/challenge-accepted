43d4e6bf867cacbf1e2f61edc685e414f70a6e69
Logan <loganxlow@gmail.com>
2016-02-25 16:38:42 -0800

    Removed challenge/task performs on client side as well

--- public/modules/to-do-list/controllers/user-to-do.client.controller.js
+++ public/modules/to-do-list/controllers/user-to-do.client.controller.js
@@ -197,14 +197,16 @@ angular.module('to-do-list').controller('UserToDoController', ['$scope', 'Authen
 
     //removeChallengeTask
       $scope.removeChallengeTask = function(challengeIndex, index){
-      Todo.removeChallengeTask(challengeIndex, index).then(function(){$scope.userChallenge[challengeIndex].splice(index,1); $scope.getUserChallenges();console.log('challenge index, index' + challengeIndex + index, $scope.userChallenge[challengeIndex]);});
-      
+      Todo.removeChallengeTask(challengeIndex, index).then(function(){$scope.getUserChallenges();console.log('challenge index, index' + challengeIndex + index, $scope.userChallenge[challengeIndex]);});
+      $scope.userChallenge[challengeIndex].splice(index,1);
      };
 
     //remove Challenge
     $scope.removeChallenge = function(id){
-      Todo.removeChallenge(id).then(function(){$scope.userChallenge.splice(id,1); $scope.getUserChallenges();console.log('removing challenge', $scope.userChallenge);});
-      ;
+      Todo.removeChallenge(id).then(function(){$scope.getUserChallenges();console.log('removing challenge', $scope.userChallenge);});
+      $scope.userChallenge.splice(id,1); 
+      console.log('challenges after removal', $scope.userChallenge);
+      
       
     };
     //Initialization function for getting initial user data
